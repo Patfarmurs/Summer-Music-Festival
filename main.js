ecee0c3d8c56a3d94d3c256513595f72bc31d5ae
unknown <patrickkeita80@yahoo.com>
2022-12-03 09:52:14 -0800

    Update main.js file

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const homeLink = document.querySelector('#homeLink');
 const aboutLink = document.querySelector('#aboutLink');
 const speakersCards = document.querySelector('.speakers-elt');
 
-function open () {
+function openMenu() {
   document.querySelector('.menuformobile').style.display = 'flex';
   document.querySelector('.closeicon').style.display = 'block';
   document.querySelector('main').style.filter = ' blur(6px)';
 }
 
-function  closed() {
+function closedMenu() {
   document.querySelector('.menuformobile').style.display = 'none';
   document.querySelector('header').style.filter = ' blur(0)';
   document.querySelector('main').style.filter = ' blur(0)';
